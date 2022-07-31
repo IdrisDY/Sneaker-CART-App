b1cf601922d6138a9d7bd000c41e22f0c6b1cfde
Idris adekunle <idrisadekunle726@gmail.com>
2022-07-31 11:24:51 +0100

    correct slider image path

--- src/Components/imageswipe/images.jsx
+++ src/Components/imageswipe/images.jsx
@@ -1,21 +1,26 @@
+import image1 from '../images/image-product-1.jpg'
+import image2 from '../images/image-product-2.jpg'
+import image3 from '../images/image-product-3.jpg'
+import image4 from '../images/image-product-4.jpg'
+
 const images = [{
    id: 1,
-   src: "./src/Components/images/image-product-1.jpg",
+   src:image1 ,
    alt: "Image 1"
 },
 {
    id: 2,
-   src: "./src/Components/images/image-product-2.jpg",
+   src: image2,
    alt: "Image 2 "
 },
 {
    id: 3,
-   src: "./src/Components/images/image-product-3.jpg",
+   src: image3,
    alt: "Image 3"
 },
 {
    id: 4,
-   src: "./src/Components/images/image-product-4.jpg",
+   src:image4,
    alt: "Image 4"
 }
 
